5b55c580e22b242b6e9d70e09a8ff75077597703
Gopal Kumar <31614501+Krgopal@users.noreply.github.com>
2024-04-08 07:20:27 +0530

    Time: 30 ms (79.59%) | Memory: 56.7 MB (50.41%) - LeetSync

--- practice/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ practice/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestSubarray(int[] nums, int limit) {
+    public int longestSubarray1(int[] nums, int limit) {
         int left = 0;
         TreeSet<Integer> set = new TreeSet<>((a, b) -> nums[a] == nums[b] ? a - b : nums[a] - nums[b]);
         set.add(0);
@@ -12,5 +12,23 @@ class Solution {
             res = Math.max(res, right - left + 1);
         }
         return res;
+    }
+
+    public int longestSubarray(int[] A, int limit) {
+        Deque<Integer> maxd = new ArrayDeque<>();
+        Deque<Integer> mind = new ArrayDeque<>();
+        int i = 0, j;
+        for (j = 0; j < A.length; ++j) {
+            while (!maxd.isEmpty() && A[j] > maxd.peekLast()) maxd.pollLast();
+            while (!mind.isEmpty() && A[j] < mind.peekLast()) mind.pollLast();
+            maxd.add(A[j]);
+            mind.add(A[j]);
+            if (maxd.peek() - mind.peek() > limit) {
+                if (maxd.peek() == A[i]) maxd.poll();
+                if (mind.peek() == A[i]) mind.poll();
+                ++i;
+            }
+        }
+        return j - i;
     }
 }
