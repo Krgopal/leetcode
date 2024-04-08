fb87b82accd1a5b3b1be6b9c0dcd4482e3ba76ca
Gopal Kumar <31614501+Krgopal@users.noreply.github.com>
2024-04-08 07:40:20 +0530

    Time: 25 ms (96.39%) | Memory: 56.4 MB (68.77%) - LeetSync

--- practice/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ practice/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,34 +1,27 @@
 class Solution {
-    public int longestSubarray1(int[] nums, int limit) {
-        int left = 0;
-        TreeSet<Integer> set = new TreeSet<>((a, b) -> nums[a] == nums[b] ? a - b : nums[a] - nums[b]);
-        set.add(0);
-        int res = 1;
-        for (int right = 1; right < nums.length; right++) {
-            set.add(right);
-            while (nums[set.last()] - nums[set.first()] > limit) {
-                set.remove(left++);
+    public int longestSubarray(int[] nums, int limit) {
+        ArrayDeque<Integer> maxd= new ArrayDeque<>();
+        ArrayDeque<Integer> mind = new ArrayDeque<>();
+        int i=0, j;
+        for (j=0; j<nums.length; j++) {
+            while (!maxd.isEmpty() && nums[j] > maxd.peekLast()) {
+                maxd.pollLast();
             }
-            res = Math.max(res, right - left + 1);
-        }
-        return res;
-    }
-
-    public int longestSubarray(int[] A, int limit) {
-        Deque<Integer> maxd = new ArrayDeque<>();
-        Deque<Integer> mind = new ArrayDeque<>();
-        int i = 0, j;
-        for (j = 0; j < A.length; ++j) {
-            while (!maxd.isEmpty() && A[j] > maxd.peekLast()) maxd.pollLast();
-            while (!mind.isEmpty() && A[j] < mind.peekLast()) mind.pollLast();
-            maxd.add(A[j]);
-            mind.add(A[j]);
+            while (!mind.isEmpty() && nums[j] < mind.peekLast()) {
+                mind.pollLast();
+            }
+            maxd.add(nums[j]);
+            mind.add(nums[j]);
             if (maxd.peek() - mind.peek() > limit) {
-                if (maxd.peek() == A[i]) maxd.poll();
-                if (mind.peek() == A[i]) mind.poll();
-                ++i;
+                if (maxd.peek() == nums[i]) {
+                    maxd.poll();
+                }
+                if (mind.peek() == nums[i]) {
+                    mind.poll();
+                }
+                i++;
             }
         }
-        return j - i;
+        return j -i;
     }
 }
