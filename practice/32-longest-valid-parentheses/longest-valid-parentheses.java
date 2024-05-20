adf0394712d644b5f89dc15c942ff7ad0327cf09
Gopal Kumar <31614501+Krgopal@users.noreply.github.com>
2024-05-20 19:09:25 +0530

    Time: 3 ms (67.32%) | Memory: 43.2 MB (14.21%) - LeetSync

--- practice/32-longest-valid-parentheses/longest-valid-parentheses.java
+++ practice/32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestValidParentheses(String s) {
+    public int longestValidParenthesesdp(String s) {
         int maxLength = 0;
         int[] dp = new int[s.length()];
         for (int i=1; i<s.length(); i++) {
@@ -13,5 +13,23 @@ class Solution {
             }
         }
         return maxLength;
+    }
+
+    public int longestValidParentheses(String s) {
+        int maxAns = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                }
+                maxAns = Math.max(maxAns, i - stack.peek());
+            }
+        }
+        return maxAns;
     }
 }
