00b1ea2529b8b19170241ce36dc829816c3cc654
Gopal Kumar <31614501+Krgopal@users.noreply.github.com>
2024-06-26 18:01:16 +0530

    Time: 43 ms (83.51%) | Memory: 124 MB (17.86%) - LeetSync

--- practice/146-lru-cache/lru-cache.java
+++ practice/146-lru-cache/lru-cache.java
@@ -1,68 +1,68 @@
 class LRUCache {
     int capacity;
+    Map<Integer, ListNode> map;
     ListNode head, tail;
-    HashMap<Integer, ListNode> dic;
+
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        dic = new HashMap<>();
-        head = new ListNode(-1, -1);
-        tail = new ListNode(-1, -1);
+        this.map = new HashMap<>();
+        this.head = new ListNode(-1, -1);
+        this.tail = new ListNode(-1, -1);
         head.next = tail;
         tail.prev = head;
     }
+
+    private void add(ListNode node) {
+        ListNode headStart = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = headStart;
+        headStart.prev = node;
+    }
+
+    private void remove(ListNode node) {
+        ListNode prev = node.prev;
+        prev.next = node.next;
+        node.next.prev = prev;
+    }
     
     public int get(int key) {
-        if (!dic.containsKey(key)) {
+        if(!map.containsKey(key)) {
             return -1;
         }
-        
-        ListNode node = dic.get(key);
+        ListNode node = map.get(key);
         remove(node);
         add(node);
         return node.val;
     }
-
-    public void add(ListNode node) {
-        ListNode previousEnd = tail.prev;
-        previousEnd.next = node;
-        node.prev = previousEnd;
-        node.next = tail;
-        tail.prev = node;
-    }
-    
-    public void remove(ListNode node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
     
     public void put(int key, int value) {
-        if (dic.containsKey(key)) {
-            ListNode oldNode = dic.get(key);
-            remove(oldNode);
-        }
-    
+        if (map.containsKey(key)) {
+            ListNode node = map.get(key);
+            remove(node);
+        } 
         ListNode node = new ListNode(key, value);
-        dic.put(key, node);
+        map.put(key, node);
         add(node);
-        
-        if (dic.size() > capacity) {
-            ListNode nodeToDelete = head.next;
-            remove(nodeToDelete);
-            dic.remove(nodeToDelete.key);
+        if (map.size() > capacity) {
+            ListNode nodeToBeDeleted = tail.prev;
+            remove(nodeToBeDeleted);
+            map.remove(nodeToBeDeleted.key);
         }
     }
 }
-class ListNode {
+class ListNode{
     int key;
     int val;
-    ListNode next;
     ListNode prev;
-
+    ListNode next;
     public ListNode(int key, int val) {
         this.key = key;
         this.val = val;
     }
 }
+
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
