a40456d889add5a95521320b72125beea36be3f9
Gopal Kumar <31614501+Krgopal@users.noreply.github.com>
2024-07-03 15:07:41 +0530

    Time: 2 ms (97.27%) | Memory: 44.4 MB (78.38%) - LeetSync

--- practice/44-wildcard-matching/wildcard-matching.java
+++ practice/44-wildcard-matching/wildcard-matching.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int sIndex = 0, pIndex = 0, matchIndex = 0, starIndex = -1;
-        
-        while (sIndex < s.length()) {
+        int pIndex = 0, sIndex = 0, matchIndex = 0, starIndex = -1;
+        while(sIndex < s.length()) {
             if (pIndex < p.length() && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '?')) {
                 sIndex++;
                 pIndex++;
@@ -11,18 +10,16 @@ class Solution {
                 matchIndex = sIndex;
                 pIndex++;
             } else if (starIndex != -1) {
-                pIndex = starIndex + 1;
+                pIndex = starIndex +1;
                 matchIndex++;
                 sIndex = matchIndex;
             } else {
                 return false;
             }
         }
-        
-        while (pIndex < p.length() && p.charAt(pIndex) == '*') {
+        while (pIndex <p.length() && p.charAt(pIndex) == '*') {
             pIndex++;
         }
-        
         return pIndex == p.length();
     }
 }
